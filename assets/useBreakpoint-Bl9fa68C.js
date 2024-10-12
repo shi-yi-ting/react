4d584f6c59f18954566a0a243d21197b62591b5a
拾亦婷 <1032731503@qq.com>
2024-10-12 09:03:49 +0800

    20241012 Signed-off-by: 拾亦婷 <1032731503@qq.com>

---
+++ assets/useBreakpoint-Bl9fa68C.js
@@ -0,0 +1,1 @@
+import{r as o,bN as a,$ as c}from"./index-Dr_4phEI.js";import{u as f}from"./useForceUpdate-EqtrIU-x.js";function b(){let s=arguments.length>0&&arguments[0]!==void 0?arguments[0]:!0;const e=o.useRef({}),t=f(),r=a();return c(()=>{const n=r.subscribe(u=>{e.current=u,s&&t()});return()=>r.unsubscribe(n)},[]),e.current}export{b as u};
