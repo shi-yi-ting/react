cb3353d3833094146fe9fd2767f9ca287df4f090
拾亦婷 <1032731503@qq.com>
2024-10-12 09:10:25 +0800

    202410120910 Signed-off-by: 拾亦婷 <1032731503@qq.com>

---
+++ assets/context-DsFCtrnf.js
@@ -0,0 +1,1 @@
+import{r as t,H as c}from"./index-DD7eXg8_.js";function s(){}const u=t.createContext({add:s,remove:s});function m(n){const r=t.useContext(u),o=t.useRef();return c(e=>{if(e){const a=n?e.querySelector(n):e;r.add(a),o.current=a}else r.remove(o.current)})}export{m as u};
